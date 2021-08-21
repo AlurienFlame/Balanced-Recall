7691d133c00cc711743936e85c77887610c010a0
AlurienFlame <62194841+AlurienFlame@users.noreply.github.com>
2021-08-22 08:19:16 +1200

    Initial Commit

---
+++ src/main/java/net/balancedrecall/BalancedRecall.java
@@ -0,0 +1,10 @@
+package net.balancedrecall;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BalancedRecall implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Hello Balanced Recall World!");
+    }
+}
