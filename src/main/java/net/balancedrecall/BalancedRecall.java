0048a22b23053d1dd542627fda36f00dbbd8117f
AlurienFlame <62194841+AlurienFlame@users.noreply.github.com>
2021-08-25 14:47:48 +1200

    Added item animation when recalling

--- src/main/java/net/balancedrecall/BalancedRecall.java
+++ src/main/java/net/balancedrecall/BalancedRecall.java
@@ -2,8 +2,12 @@ package net.balancedrecall;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.client.model.FabricModelPredicateProviderRegistry;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.RecipeSerializer;
 import net.minecraft.stat.StatFormatter;
 import net.minecraft.stat.Stats;
@@ -31,6 +35,20 @@ public class BalancedRecall implements ModInitializer {
         Registry.register(Registry.ITEM, new Identifier(MODID, "magic_mirror"), MAGIC_MIRROR);
         Registry.register(Registry.ITEM, new Identifier(MODID, "dimensional_mirror"), DIMENSIONAL_MIRROR);
         Registry.register(Registry.ITEM, new Identifier(MODID, "sleeping_mat"), SLEEPING_MAT);
+
+        // Model Predicates
+        FabricModelPredicateProviderRegistry.register(MAGIC_MIRROR, new Identifier("recalling"), (ItemStack itemStack, ClientWorld clientWorld, LivingEntity livingEntity, int seed) -> {
+            if (livingEntity == null || livingEntity.getActiveItem() != itemStack) {
+                return 0.0F;
+            }
+            return (itemStack.getMaxUseTime() - livingEntity.getItemUseTimeLeft()) / 20.0F;
+        });
+        FabricModelPredicateProviderRegistry.register(DIMENSIONAL_MIRROR, new Identifier("recalling"), (ItemStack itemStack, ClientWorld clientWorld, LivingEntity livingEntity, int seed) -> {
+            if (livingEntity == null || livingEntity.getActiveItem() != itemStack) {
+                return 0.0F;
+            }
+            return (itemStack.getMaxUseTime() - livingEntity.getItemUseTimeLeft()) / 20.0F;
+        });
 
         // Recipe
         Registry.register(Registry.RECIPE_SERIALIZER, new Identifier(MODID, "mirror_repairing"), MIRROR_REPAIRING_SERIALIZER);
