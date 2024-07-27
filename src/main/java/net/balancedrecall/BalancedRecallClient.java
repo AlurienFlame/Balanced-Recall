20b42d08d2bac46255622a6d96886f7729b59383
AlurienFlame <62194841+AlurienFlame@users.noreply.github.com>
2024-07-28 10:21:15 +1200

    neater model predicate code

--- src/main/java/net/balancedrecall/BalancedRecallClient.java
+++ src/main/java/net/balancedrecall/BalancedRecallClient.java
@@ -2,6 +2,9 @@ package net.balancedrecall;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.client.item.ModelPredicateProviderRegistry;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 
 public class BalancedRecallClient implements ClientModInitializer{
@@ -9,9 +12,23 @@ public class BalancedRecallClient implements ClientModInitializer{
     @Override
     public void onInitializeClient() {
         // Model Predicates
-        ModelPredicateProviderRegistry.register(BalancedRecall.MAGIC_MIRROR, Identifier.of("recalling"), new MirrorRecallingModelPredicateProvider());
-        ModelPredicateProviderRegistry.register(BalancedRecall.DIMENSIONAL_MIRROR, Identifier.of("recalling"), new MirrorRecallingModelPredicateProvider());
-        ModelPredicateProviderRegistry.register(BalancedRecall.MAGIC_MIRROR, Identifier.of("broken"), new MirrorBrokenModelPredicateProvider());
-        ModelPredicateProviderRegistry.register(BalancedRecall.DIMENSIONAL_MIRROR, Identifier.of("broken"), new MirrorBrokenModelPredicateProvider());
+        ModelPredicateProviderRegistry.register(BalancedRecall.MAGIC_MIRROR, Identifier.of("recalling"), (ItemStack itemStack, ClientWorld clientWorld, LivingEntity livingEntity, int seed) -> {
+            if (livingEntity == null || livingEntity.getActiveItem() != itemStack) {
+                return 0.0F;
+            }
+            return (itemStack.getMaxUseTime(livingEntity) - livingEntity.getItemUseTimeLeft()) / 20.0F;
+        });
+        ModelPredicateProviderRegistry.register(BalancedRecall.DIMENSIONAL_MIRROR, Identifier.of("recalling"), (ItemStack itemStack, ClientWorld clientWorld, LivingEntity livingEntity, int seed) -> {
+            if (livingEntity == null || livingEntity.getActiveItem() != itemStack) {
+                return 0.0F;
+            }
+            return (itemStack.getMaxUseTime(livingEntity) - livingEntity.getItemUseTimeLeft()) / 20.0F;
+        });
+        ModelPredicateProviderRegistry.register(BalancedRecall.MAGIC_MIRROR, Identifier.of("broken"), (ItemStack itemStack, ClientWorld clientWorld, LivingEntity livingEntity, int seed) -> {
+            return (itemStack.getDamage() >= itemStack.getMaxDamage() - 1) ? 1F : 0F;
+        });
+        ModelPredicateProviderRegistry.register(BalancedRecall.DIMENSIONAL_MIRROR, Identifier.of("broken"), (ItemStack itemStack, ClientWorld clientWorld, LivingEntity livingEntity, int seed) -> {
+            return (itemStack.getDamage() >= itemStack.getMaxDamage() - 1) ? 1F : 0F;
+        });
     }
 }
