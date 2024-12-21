f5e7147d40375970bc8f4c2359cfb0eaa2ce3953
AlurienFlame <62194841+AlurienFlame@users.noreply.github.com>
2024-12-20 19:26:01 -0800

    layout is... adequate

--- src/main/java/net/balancedrecall/ConfigScreen.java
+++ src/main/java/net/balancedrecall/ConfigScreen.java
@@ -8,6 +8,7 @@ import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.CheckboxWidget;
 import net.minecraft.client.gui.widget.ClickableWidget;
 import net.minecraft.client.gui.widget.DirectionalLayoutWidget;
+import net.minecraft.client.gui.widget.GridWidget;
 import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.client.gui.widget.TextWidget;
 import net.minecraft.client.gui.widget.ThreePartsLayoutWidget;
@@ -15,7 +16,7 @@ import net.minecraft.text.Text;
 
 public class ConfigScreen extends Screen {
     private Screen parent;
-	private final ThreePartsLayoutWidget layout = new ThreePartsLayoutWidget(this, 61, 33);
+	private final ThreePartsLayoutWidget layout = new ThreePartsLayoutWidget(this);
 
 	public ConfigScreen(@Nullable Screen parent) {
         super(Text.translatable("balancedrecall.menu.title"));
@@ -35,29 +36,39 @@ public class ConfigScreen extends Screen {
 		// TODO: detect config in real code
 
 		DirectionalLayoutWidget column = DirectionalLayoutWidget.vertical().spacing(8);
-		column.getMainPositioner().alignHorizontalCenter();
+
+		layout.addHeader(new TextWidget(Text.of("Balanced Recall Configuration"), this.textRenderer));
 
 		// Mirror settings
 		column.add(CheckboxWidget.builder(Text.of("Taking damage interrupts recall"), this.textRenderer).checked(true).build());
 		column.add(CheckboxWidget.builder(Text.of("Taking damage puts mirror on cooldown"), this.textRenderer).checked(true).build());
 		column.add(CheckboxWidget.builder(Text.of("Recalling is impossible when mosters are nearby"), this.textRenderer).checked(true).build());
 
-		// column.add(new TextWidget(Text.of("Magic Mirror Use Time"), textRenderer));
-		column.add(new TextFieldWidget(textRenderer, 100, 20, Text.of("Magic Mirror Use Time")));
-		column.add(new TextFieldWidget(textRenderer, 100, 20, Text.of("Magic Mirror Cooldown Time")));
-		column.add(new TextFieldWidget(textRenderer, 100, 20, Text.of("Dimensional Mirror Use Time")));
-		column.add(new TextFieldWidget(textRenderer, 100, 20, Text.of("Dimensional Mirror Cooldown Time")));
+		GridWidget grid = new GridWidget();
+		grid.getMainPositioner().margin(4).alignHorizontalCenter();
+		GridWidget.Adder adder = grid.createAdder(2);
+		adder.getMainPositioner().alignLeft().alignVerticalCenter();
+		adder.add(new TextWidget(Text.of("Magic Mirror Use Time"), this.textRenderer));
+		adder.add(new TextFieldWidget(this.textRenderer, 100, 20, Text.of("Magic Mirror Use Time")));
+		adder.add(new TextWidget(Text.of("Magic Mirror Cooldown Time"), this.textRenderer));
+		adder.add(new TextFieldWidget(this.textRenderer, 100, 20, Text.of("Magic Mirror Cooldown Time")));
+		adder.add(new TextWidget(Text.of("Dimensional Mirror Use Time"), this.textRenderer));
+		adder.add(new TextFieldWidget(this.textRenderer, 100, 20, Text.of("Dimensional Mirror Use Time")));
+		adder.add(new TextWidget(Text.of("Dimensional Mirror Cooldown Time"), this.textRenderer));
+		adder.add(new TextFieldWidget(this.textRenderer, 100, 20, Text.of("Dimensional Mirror Cooldown Time")));
+		column.add(grid);
 
 		// Recall settings
 		column.add(CheckboxWidget.builder(Text.of("Sleeping mat resets phantom timer"), this.textRenderer).build());
 
-		// Config screen buttons
-		column.add(ButtonWidget.builder(Text.of("Done"), (btn) -> this.close()).dimensions(40, 40, 120, 20).build());
+		this.layout.addBody(column);
 
-		column.forEachChild(child -> {
+		// Config screen buttons
+		this.layout.addFooter(ButtonWidget.builder(Text.of("Done"), (btn) -> this.close()).dimensions(40, 40, 120, 20).build());
+
+		this.layout.forEachChild(child -> {
 			this.addDrawableChild(child);
 		});
-		this.layout.addBody(column);
 		this.refreshWidgetPositions();
 	}
 
